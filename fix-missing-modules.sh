604c28b90fb7017efdf252e147e5272a42cb4143
William Hughes <williamh@catalyst.net.nz>
2013-11-14 10:32:52 +1300

    fixed fix-missing-modules to just grab all modules

--- fix-missing-modules.sh
+++ fix-missing-modules.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "cd /vagrant/public/sites/default/modules && for f in  *; do echo \$f && sudo -E rsync -qr $(whoami)@web2:/home/drupal/contrib/\$f /home/drupal/contrib; done" | vagrant ssh
-echo "cd /vagrant/public/sites/default/libraries && for f in  *; do echo \$f && sudo -E rsync -qr $(whoami)@web2:/home/drupal/libraries/\$f /home/drupal/libraries; done" | vagrant ssh
+echo "cd /home/drupal && sudo -E rsync -qr $(whoami)@web2:/home/drupal/contrib . && sudo -E rsync -qr $(whoami)@web2:/home/drupal/libraries . " | vagrant ssh
